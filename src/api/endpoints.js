49c13c34db8c7f57c918cef135c72931a62beb94
Erlan <eabdyrakhmanovv@gmail.com>
2023-12-02 15:23:33 +0600

    changed API key

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,22 +1,20 @@
-const API_KEY = 'CRggWynztXfQbHzbHRUCH6iuJtIqGgi4';
-const BASE_API = 'https://api.apilayer.com/';
+const API_KEY = "jdEE1e30gKPVlOzybedgDtt1qtZYJY6v";
+const BASE_API = "https://api.apilayer.com/";
 
 const REQUEST_HEADERS = {
-    headers: {
-        apiKey: API_KEY
-    }
-}
+  headers: {
+    apiKey: API_KEY,
+  },
+};
 
-const fixerApi = (url) => `${BASE_API}fixer/${url}`
+const fixerApi = (url) => `${BASE_API}fixer/${url}`;
 
 const API = {
-    CURRENCY: {
-        convert: (to, from, amount) => fixerApi(`convert?to=${to}&from=${from}&amount=${amount}`),
-        symbols: fixerApi('symbols')
-    }
-}
+  CURRENCY: {
+    convert: (to, from, amount) =>
+      fixerApi(`convert?to=${to}&from=${from}&amount=${amount}`),
+    symbols: fixerApi("symbols"),
+  },
+};
 
-export {
-    REQUEST_HEADERS,
-    API
-}
+export { REQUEST_HEADERS, API };
